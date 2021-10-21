4c5e27f06f9ca64465a12a0acfaff1232faa378d
Tyler Kelleher <tyler_kelleher@hotmail.com>
2021-10-21 12:46:36 -0400

    super big commit that connects app to twitter

---
+++ app/models/tweet.rb
@@ -0,0 +1,26 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  belongs_to :twitter_account
+
+  validates :body, length: { minimum: 1, maximum: 280 }
+  validates :published_at, presence: true
+
+  after_initialize do 
+    self.published_at ||= 24.hours.from_now
+  end
+
+  after_save_commit do
+    if published_at_previously_changed?
+      TweetJob.set(wait_until: published_at).perform_later(self)
+    end
+  end
+
+  def published?
+    tweet_id?
+  end
+# not working??? getting Twitter::Error::Forbidden
+  def publish_to_twitter!
+    tweet = twitter_account.client.update(body)
+    update(tweet_id: tweet.id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :twitter_accounts
+  has_many :tweets
   has_secure_password
+
   validates :email, presence: true, format: { with: /\A(\S+)@(.+)\.(\S+)\z/, message: 'must be a vaild email address' }
 end
